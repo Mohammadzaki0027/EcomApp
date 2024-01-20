3ca63a4c2bd3880273c8edb09a0c80e80bec514d
Zaki Shaikh <zakishaikh@Zakis-MacBook-Pro.local>
2024-01-20 14:49:51 +0530

    Password And Formik Set Up is done

--- ecomapp/src/Components/SignIn.tsx
+++ ecomapp/src/Components/SignIn.tsx
@@ -3,41 +3,64 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-
+import "../App.css";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
+import { useFormik } from "formik";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const defaultTheme = createTheme();
-
+interface errorstype {
+  password?: string;
+  email?: string;
+}
 export default function SignIn() {
-  const [error, setError] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const handelBlur = () => {
-    if (!email) {
-      setError("Connot be Empty");
-    }
-  };
-  const handleChange = (e: any) => {
-    setEmail(e.target.value);
-  };
+  const[password,setPassword]=React.useState(true)
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
   };
+  const handleTogglePassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+event.stopPropagation()
+    setPassword((password) => !password);
+  };
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      email: "",
+    },
+    validate: (values) => {
+      const errors: errorstype = {};
 
+      if (!values.password) {
+        errors.password = "Required";
+      }
+
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: ".5rem",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
@@ -50,56 +73,53 @@ export default function SignIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onBlur={handelBlur}
-              onChange={handleChange}
-            />
-            {error && (
-              <Typography
-                variant="h6"
-                sx={{
-                  fontFamily: "sans-serif",
-                  color: "red",
-                  fontSize: "14px",
-                }}
-              >
-                {error}
-              </Typography>
-            )}
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
+          <Box sx={{ mt: 1 }}>
+            <form onSubmit={formik.handleSubmit}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                autoFocus
+                id="email"
+                label="Email Address"
+                autoComplete="email"
+                name="email"
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
+              <TextField
+                fullWidth
+                margin="normal"
+                id="password"
+                name="password"
+                label="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+                type={password?"password":"text"}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+            </form>
+            <Button  sx={{ mt: 1, mb: 1, width: "50%" }}  type="button" onClick={handleTogglePassword}>
+             { password?"Show Password":"Hide Password"}
             </Button>
-            <Grid container>
+            <Grid container gap={2}>
               <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
@@ -111,7 +131,24 @@ export default function SignIn() {
                 </Link>
               </Grid>
             </Grid>
+            {/* Google Sign In start Here */}
+
+            <Box
+              component="button"
+              sx={{
+                width: "100%",
+                border: "1px solid red",
+                display: "flex",
+                justifyContent: "space-evenly",
+                marginTop: "1rem",
+                padding: ".5rem",
+                backgroundColor: "white",
+                borderRadius: "1rem",
+                alignItems: "center",
+              }}
+            ></Box>
           </Box>
+          {/* Google SignIn End Here */}
         </Box>
       </Container>
     </ThemeProvider>
--- ecomapp/src/Pages/Login.tsx
+++ ecomapp/src/Pages/Login.tsx
@@ -2,17 +2,17 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import SignIn from "../Components/SignIn";
 
+
+
 const Login = () => {
   return (
     <Box
       sx={{
-        paddingTop: "120px",
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
+   
+       
         width: "100%",
         margin: "auto",
-        border: "1px solid red",
+     
       }}
     >
       <SignIn />
